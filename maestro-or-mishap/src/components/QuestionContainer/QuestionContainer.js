a8c42c27cae27679bcecbd123107adaf629ae383
Shauna Myers <shaunamyers26@gmail.com>
2021-08-04 20:40:32 +0100

    Map through data and pass to QuestionCard

--- maestro-or-mishap/src/components/QuestionContainer/QuestionContainer.js
+++ maestro-or-mishap/src/components/QuestionContainer/QuestionContainer.js
@@ -1,9 +1,15 @@
-
+import QuestionCard from '../QuestionCard/QuestionCard';
 
 const QuestionContainer = ({ questions }) => {
+
+    const allQuestions = questions[0].results.map(question => {
+        console.log('question', question)
+        return <QuestionCard key={Math.random()} question={question.question} correctAnswer={question.correct_answer} incorrectAnswer={question.incorrect_answer} />
+    })
+
     return (
         <section>
-            <p>Hello</p>
+            {allQuestions}
         </section>
     )
 }
