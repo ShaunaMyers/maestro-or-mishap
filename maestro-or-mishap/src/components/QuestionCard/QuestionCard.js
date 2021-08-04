0807d0ff9aae4a429eda77c5abdb364fc695fb76
Shauna Myers <shaunamyers26@gmail.com>
2021-08-04 20:40:03 +0100

    Build Question card with API data

---
+++ maestro-or-mishap/src/components/QuestionCard/QuestionCard.js
@@ -0,0 +1,16 @@
+
+
+const QuestionCard = ({question, correctAnswer, incorrectAnswer}) => {
+    console.log('what is this?', incorrectAnswer)
+    return(
+        <article>
+            <h2>{question}</h2>
+            <p>{correctAnswer}</p>
+            {/* <p>{incorrectAnswer[0]}</p>
+            <p>{incorrectAnswer[1]}</p>
+            <p>{incorrectAnswer[2]}</p> */}
+        </article>
+    )
+}
+
+export default QuestionCard;
