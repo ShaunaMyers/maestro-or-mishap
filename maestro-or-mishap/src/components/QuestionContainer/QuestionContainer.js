3dad0b67977b6e886018f34511dd991d4b925a6b
Shauna Myers <shaunamyers26@gmail.com>
2021-08-05 17:22:20 +0100

    Change incorrect_answer to incorrect_answers

--- maestro-or-mishap/src/components/QuestionContainer/QuestionContainer.js
+++ maestro-or-mishap/src/components/QuestionContainer/QuestionContainer.js
@@ -6,12 +6,12 @@ const QuestionContainer = ({ questions }) => {
     const allQuestions = questions.map(question => {
         console.log('questions results', questions[0].results)
         console.log('question', question)
-        return <QuestionCard key={Math.random()} question={question.question} correctAnswer={question.correct_answer} incorrectAnswer={question.incorrect_answer} />
+        return <QuestionCard key={Math.random()} question={question.question} correctAnswer={question.correct_answer} incorrectAnswer={question.incorrect_answers} />
     })
 
     return (
         <section>
-            {/* {allQuestions} */}
+            {allQuestions}
         </section>
     )
 }
