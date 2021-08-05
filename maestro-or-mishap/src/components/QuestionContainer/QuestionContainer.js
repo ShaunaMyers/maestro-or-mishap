3b74cff52644c1391af2c8b90eadd5b08f19d8e5
Shauna Myers <shaunamyers26@gmail.com>
2021-08-05 17:17:58 +0100

    Map over correct questions array

--- maestro-or-mishap/src/components/QuestionContainer/QuestionContainer.js
+++ maestro-or-mishap/src/components/QuestionContainer/QuestionContainer.js
@@ -1,15 +1,17 @@
 import QuestionCard from '../QuestionCard/QuestionCard';
 
 const QuestionContainer = ({ questions }) => {
+    console.log('questions results', questions)
 
-    const allQuestions = questions[0].results.map(question => {
+    const allQuestions = questions.map(question => {
+        console.log('questions results', questions[0].results)
         console.log('question', question)
         return <QuestionCard key={Math.random()} question={question.question} correctAnswer={question.correct_answer} incorrectAnswer={question.incorrect_answer} />
     })
 
     return (
         <section>
-            {allQuestions}
+            {/* {allQuestions} */}
         </section>
     )
 }
