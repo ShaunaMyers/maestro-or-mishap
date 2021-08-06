24fe9e3484a8698564faf1aa057f12fab841183f
Shauna Myers <shaunamyers26@gmail.com>
2021-08-06 15:51:49 +0100

    Build out Greeting JSX with greeting message

---
+++ maestro-or-mishap/src/components/Greeting/Greeting.js
@@ -0,0 +1,12 @@
+
+const Greeting = () => {
+    return(
+        <section>
+            <h2>Welcome!</h2>
+            <p>Please try your hand at our music trivia game and see if you are a Maestro or if you land in Mishap!</p>
+            <button>Start Game!</button>
+        </section>
+    )
+}
+
+export default Greeting;
