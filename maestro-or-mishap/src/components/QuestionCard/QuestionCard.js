bd9ca3c8c41752b1a67e1263770b8e3078078acb
Shauna Myers <shaunamyers26@gmail.com>
2021-08-07 17:46:38 +0100

    Fix logic to switch questions

--- maestro-or-mishap/src/components/QuestionCard/QuestionCard.js
+++ maestro-or-mishap/src/components/QuestionCard/QuestionCard.js
@@ -2,18 +2,21 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const QuestionCard = ({currentIndex, question, correctAnswer, incorrectAnswer}) => {
-    console.log('incorrect answer', incorrectAnswer)
+    console.log('current index', currentIndex)
+    console.log('questionCard question', question)
 
     const [gameOver, setGameOver] = useState(false);
     const [nextIndex, setNextIndex] = useState(0);
     
     const formatIndex = () => {
+        console.log('current Index plus 1', currentIndex + 1)
+        // currentIndex < 12 ? setNextIndex(currentIndex + 1) : 
+        // setGameOver(true)
         if (currentIndex < 12) {
-            const indexSum = currentIndex + 1;
-            setNextIndex(indexSum);
-            // return nextIndex;
-        } else {
-            setGameOver(true);
+            const indexSummed = (currentIndex + 1);
+            console.log(indexSummed, 'index summed')
+            setNextIndex(indexSummed)
+            console.log('setNextIndex', setNextIndex)
         }
     }
 
@@ -24,7 +27,11 @@ const QuestionCard = ({currentIndex, question, correctAnswer, incorrectAnswer}) 
             <p>{incorrectAnswer[0]}</p>
             <p>{incorrectAnswer[1]}</p>
             <p>{incorrectAnswer[2]}</p>
-            <Link to={`/question/${nextIndex}`}><button onClick={formatIndex}>Next Question</button></Link>
+            {gameOver === false &&
+            <Link to={`/question/${nextIndex}`}><button onClick={formatIndex}>Next Question</button></Link> 
+            // : <Link to={`/question/${nextIndex}`}><button onClick={formatIndex}>Next Question</button></Link>
+            }
+            
         </article>
     )
 }
