61fe6e638f1b74456aebdb7c2a1ec267b80af28b
Shauna Myers <shaunamyers26@gmail.com>
2021-08-07 14:46:46 +0100

    Remove map and pass props to QuestionCard

--- maestro-or-mishap/src/components/QuestionContainer/QuestionContainer.js
+++ maestro-or-mishap/src/components/QuestionContainer/QuestionContainer.js
@@ -1,17 +1,18 @@
 import QuestionCard from '../QuestionCard/QuestionCard';
 
-const QuestionContainer = ({ questions }) => {
-    console.log('questions results', questions)
+const QuestionContainer = ({ question }) => {
+    console.log('question???', question)
 
-    const allQuestions = questions.map(question => {
-        console.log('questions results', questions[0].results)
-        console.log('question', question)
-        return <QuestionCard key={Math.random()} question={question.question} correctAnswer={question.correct_answer} incorrectAnswer={question.incorrect_answers} />
-    })
+    // const allQuestions = questions.map(question => {
+    //     console.log('questions results', questions[0].results)
+    //     console.log('question', question)
+    //     return <QuestionCard key={Math.random()} question={question.question} correctAnswer={question.correct_answer} incorrectAnswer={question.incorrect_answers} />
+    // })
 
     return (
         <section>
-            {allQuestions}
+            <QuestionCard 
+            question={question.question} correctAnswer={question.correct_answer} incorrectAnswer={question.incorrect_answers} />
         </section>
     )
 }
