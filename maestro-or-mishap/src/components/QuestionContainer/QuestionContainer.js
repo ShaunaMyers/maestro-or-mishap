b998a72a17b1066e69179c23a29b67b2260f8f0e
Shauna Myers <shaunamyers26@gmail.com>
2021-08-07 14:48:13 +0100

    Remove console logs and comments

--- maestro-or-mishap/src/components/QuestionContainer/QuestionContainer.js
+++ maestro-or-mishap/src/components/QuestionContainer/QuestionContainer.js
@@ -1,13 +1,6 @@
 import QuestionCard from '../QuestionCard/QuestionCard';
 
 const QuestionContainer = ({ question }) => {
-    console.log('question???', question)
-
-    // const allQuestions = questions.map(question => {
-    //     console.log('questions results', questions[0].results)
-    //     console.log('question', question)
-    //     return <QuestionCard key={Math.random()} question={question.question} correctAnswer={question.correct_answer} incorrectAnswer={question.incorrect_answers} />
-    // })
 
     return (
         <section>
