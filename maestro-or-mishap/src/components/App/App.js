0c59ed4630fa52ffa96e0c50b652a024469ea929
Shauna Myers <shaunamyers26@gmail.com>
2021-08-07 14:47:40 +0100

    Remove HandleDisplayQuestion & state

--- maestro-or-mishap/src/components/App/App.js
+++ maestro-or-mishap/src/components/App/App.js
@@ -7,26 +7,12 @@ import Greeting from '../Greeting/Greeting';
 
 const App = () => {
   const [questions, setQuestions] = useState([]);
-  // const [currentQuestion, setCurrentQuestion] = useState({});
 
   useEffect(() => {
     fetchQuestions() 
-      // .then(data => console.log('setQuestions', data.results))
       .then(data => setQuestions(data.results))
       .catch(error => console.log(error))
   }, [])
-
-  // const handleDisplayQuestion = (index) => {
-  //   // When user clicks Greeting button this func fires
-  //     // Pass in an index number
-  //   // We start with question at index num in questions array
-  //   setCurrentQuestion(questions[index])
-  //   // .then(console.log('curr quest', currentQuestion))
-  //   console.log('curr quest', currentQuestion)
-  //   // So, we pass that question into our QuestionContainer
-  //   // From there, it displays that question through QuestionCard
-  //   // Need to have a button on questionCard to click to get to the next question
-  // }
 
   return (
     <main className="App">
