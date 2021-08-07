f0b1d1e7345e0b7dd6f2282fb2752726e18b1203
Shauna Myers <shaunamyers26@gmail.com>
2021-08-07 15:02:23 +0100

    Add state and formatIndex func

--- maestro-or-mishap/src/components/QuestionCard/QuestionCard.js
+++ maestro-or-mishap/src/components/QuestionCard/QuestionCard.js
@@ -1,6 +1,13 @@
+import { useState } from 'react';
 
+const QuestionCard = ({index, question, correctAnswer, incorrectAnswer}) => {
 
-const QuestionCard = ({question, correctAnswer, incorrectAnswer}) => {
+    const [gameOver, setGameOver] = useState(false);
+    
+    const formatIndex = () => {
+        index < 12 ? index + 1 : setGameStatus(true);
+    }
+
     return(
         <article>
             <h2>{question}</h2>
@@ -8,6 +15,7 @@ const QuestionCard = ({question, correctAnswer, incorrectAnswer}) => {
             <p>{incorrectAnswer[0]}</p>
             <p>{incorrectAnswer[1]}</p>
             <p>{incorrectAnswer[2]}</p>
+            <Link to={`/question/${index + 1}`}><button>Next Question</button></Link>
         </article>
     )
 }
