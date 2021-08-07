f97dc932451dcd8cbd3cc4aff20e831f44dc56cc
Shauna Myers <shaunamyers26@gmail.com>
2021-08-07 14:35:43 +0100

    Add Link to question at index 0

--- maestro-or-mishap/src/components/Greeting/Greeting.js
+++ maestro-or-mishap/src/components/Greeting/Greeting.js
@@ -1,10 +1,12 @@
+import { Route, NavLink, Switch, Link } from 'react-router-dom';
 
 const Greeting = ({handleDisplayQuestion}) => {
     return(
         <section>
             <h2>Welcome!</h2>
             <p>Please try your hand at our music trivia game and see if you are a Maestro or if you land in Mishap!</p>
-            <button onClick={() => handleDisplayQuestion(0)}>Start Game!</button>
+            {/* <button onClick={() => handleDisplayQuestion(0)}>Start Game!</button> */}
+            <Link exact to='/question/0' activeClassName='greeting-btn-clicked' className='greeting-btn'>Start Game!</Link>
         </section>
     )
 }
