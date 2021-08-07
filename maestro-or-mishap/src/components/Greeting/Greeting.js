9183667fdf84cea03383d6fa3687d4349904df1f
Shauna Myers <shaunamyers26@gmail.com>
2021-08-07 14:00:47 +0100

    Add onClick function and pass in props

--- maestro-or-mishap/src/components/Greeting/Greeting.js
+++ maestro-or-mishap/src/components/Greeting/Greeting.js
@@ -1,10 +1,10 @@
 
-const Greeting = () => {
+const Greeting = ({handleDisplayQuestion}) => {
     return(
         <section>
             <h2>Welcome!</h2>
             <p>Please try your hand at our music trivia game and see if you are a Maestro or if you land in Mishap!</p>
-            <button>Start Game!</button>
+            <button onClick={handleDisplayQuestion}>Start Game!</button>
         </section>
     )
 }
