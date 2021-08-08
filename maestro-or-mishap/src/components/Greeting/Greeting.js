2b382929ad0ac4f625b1bab0d5a61bc76f573bfd
Shauna Myers <shaunamyers26@gmail.com>
2021-08-08 12:24:53 +0100

    Change greeting message

--- maestro-or-mishap/src/components/Greeting/Greeting.js
+++ maestro-or-mishap/src/components/Greeting/Greeting.js
@@ -4,7 +4,7 @@ const Greeting = ({handleDisplayQuestion}) => {
     return(
         <section>
             <h2>Welcome!</h2>
-            <p>Please try your hand at our music trivia game and see if you are a Maestro or if you land in Mishap!</p>
+            <p>Please try your hand at our music trivia game and see if you are a music maestro!</p>
             {/* <button onClick={() => handleDisplayQuestion(0)}>Start Game!</button> */}
             <Link to='/question/0'><button>Start Game!</button></Link>
         </section>
