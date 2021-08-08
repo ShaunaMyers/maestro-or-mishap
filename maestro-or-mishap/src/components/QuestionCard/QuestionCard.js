bc4dd52f9fc8be13a6b349c08fc3f3b3990db929
Shauna Myers <shaunamyers26@gmail.com>
2021-08-08 11:05:44 +0100

    change props to incorrectAnswers to allAnswers

--- maestro-or-mishap/src/components/QuestionCard/QuestionCard.js
+++ maestro-or-mishap/src/components/QuestionCard/QuestionCard.js
@@ -1,37 +1,63 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-const QuestionCard = ({currentIndex, question, correctAnswer, incorrectAnswers}) => {
-    console.log('current index', currentIndex)
-    console.log('questionCard question', question)
+const QuestionCard = ({currentIndex, question, correctAnswer, allAnswers}) => {
+    // console.log('current index', currentIndex)
+    // console.log('questionCard question', question)
 
     const [gameOver, setGameOver] = useState(false);
     const [nextIndex, setNextIndex] = useState(0);
-    const [allAnswers, setAllAnswers] = useState([...incorrectAnswers, correctAnswer]);
+    // const [allAnswers, setAllAnswers] = useState([]);
 
-    useEffect(() => {
-        // setAllAnswers([...incorrectAnswers, correctAnswer])
-        formatAnswers()
-    }, [])
+    // useEffect(() => {
+    //     console.log('Getting in here?')
+    //     let unshuffled = [...incorrectAnswers, correctAnswer]
+    
+    //     let shuffled = unshuffled
+    //     .map((value) => ({ value, sort: Math.random() }))
+    //     .sort((a, b) => a.sort - b.sort)
+    //     .map(({ value }) => value)
+    //     setAllAnswers(shuffled)
+    // }, [])
 
-    const formatAnswers = () => {
-        // setAllAnswers([...incorrectAnswers, correctAnswer])
-        let mixedAnswers = []
-        allAnswers.forEach(answer => {
-            let randomItem = allAnswers[Math.floor(Math.random()*allAnswers.length)];
-            console.log(randomItem, 'random item')
-            mixedAnswers.push(randomItem)
-        })
-        setAllAnswers(mixedAnswers)
-    }
+    // const formatAnswers = () => {
+    //     // setAllAnswers([...incorrectAnswers, correctAnswer])
+    //     // let mixedAnswers = []
+    //     // let indexNumbers = [0, 1, 2, 3]
+
+    //     // allAnswers.forEach(answer => {
+    //     //     let randomIndex;
+    //     //     if (indexNumbers.length > 1) {
+    //     //         randomIndex = indexNumbers[Math.floor(Math.random()*indexNumbers.length)];
+    //     //         console.log(randomIndex, 'random index #')
+    //     //         indexNumbers.splice(randomIndex, 1)
+    //     //         console.log(indexNumbers, 'index number array')
+    //     //     } else {
+    //     //         randomIndex = indexNumbers[0];
+    //     //         console.log(randomIndex, 'last random index')
+    //     //     }
+    //     //     mixedAnswers.push(allAnswers[randomIndex])
+    //     // })
+
+    //     // setAllAnswers(mixedAnswers)
+    //     console.log('Getting in here?')
+    //     let unshuffled = [...incorrectAnswers, correctAnswer]
+    
+    //     let shuffled = unshuffled
+    //     .map((value) => ({ value, sort: Math.random() }))
+    //     .sort((a, b) => a.sort - b.sort)
+    //     .map(({ value }) => value)
+    //     setAllAnswers(shuffled)
+    //     return allAnswers[0]
+    // }
     
     const formatIndex = () => {
-        console.log('current Index plus 1', currentIndex + 1)
+        // console.log('current Index plus 1', currentIndex + 1)
         if (currentIndex < 11) {
             const indexSummed = (currentIndex + 1);
-            console.log(indexSummed, 'index summed')
+            // console.log(indexSummed, 'index summed')
             setNextIndex(indexSummed)
-            console.log('setNextIndex', setNextIndex)
+            // console.log('setNextIndex', setNextIndex)
         } else {
             setNextIndex(11)
             setGameOver(true)
@@ -63,7 +89,7 @@ const QuestionCard = ({currentIndex, question, correctAnswer, incorrectAnswers})
             <Link to={`/question/${nextIndex}`}>
                 <button onClick={() => {
                 formatIndex()
-                formatAnswers()
+                // formatAnswers()
                 }}
             >Next Question</button></Link> :
             <Link to={'/'}>
