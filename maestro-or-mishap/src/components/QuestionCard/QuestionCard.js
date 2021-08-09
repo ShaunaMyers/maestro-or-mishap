eb8980bb66c0593af40c87848d7b56218e3c3db4
Shauna Myers <shaunamyers26@gmail.com>
2021-08-09 22:33:18 +0100

    Remove checkedResponse & add checked to state

--- maestro-or-mishap/src/components/QuestionCard/QuestionCard.js
+++ maestro-or-mishap/src/components/QuestionCard/QuestionCard.js
@@ -1,36 +1,46 @@
-import { useState} from 'react';
+import { useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 import './QuestionCard.css';
 import ScoreBox from '../ScoreBox/ScoreBox';
 
 const QuestionCard = ({currentIndex, question, correctAnswer, allAnswers, addToFinalScore}) => {
    
-
+    // const [question1, setQuestion1] = useState('');
     const [gameOver, setGameOver] = useState(false);
     const [nextIndex, setNextIndex] = useState(0);
     const [answerFeedback, setAnswerFeedback] = useState('');
     const [timer, setTimer] = useState('');
     const [score, setScore] = useState(0);
+    const [nextQuestionBtnDisabled, setNextQuestionBtnDisabled] = useState(true);
+    const [checked1, setChecked1] = useState(false);
+    const [checked2, setChecked2] = useState(false);
+    const [checked3, setChecked3] = useState(false);
+    const [checked4, setChecked4] = useState(false);
 
-    const checkResponse = () => {
-        if (!answerFeedback) {
-            setAnswerFeedback('Please select an answer.');
-            setNextIndex(currentIndex);
-            setTimer(setTimeout(() => setAnswerFeedback(''), 3000))
-        } else {
-            setAnswerFeedback('');
-            formatIndex();
-        }
-    }
+    // const checkResponse = () => {
+    //     if (!answerFeedback) {
+    //         // setAnswerFeedback('Please select an answer.');
+    //         // setNextIndex(currentIndex);
+    //         // setTimer(setTimeout(() => setAnswerFeedback(''), 3000))
+    //     } else {
+    //         setAnswerFeedback('');
+    //         formatIndex();
+    //     }
+    // }
+
+    // useEffect(() => {
+    //     setQuestion1(question1);
+    // }, [question1])
 
     const formatIndex = () => {
         if (currentIndex < 11) {
             const indexSummed = (currentIndex + 1);
-            setNextIndex(indexSummed);
+            setNextIndex(indexSummed); // ++
         } else {
             setNextIndex(11);
             setGameOver(true);
         }
+        clearCheckboxes();
     }
 
 
@@ -43,16 +53,36 @@ const QuestionCard = ({currentIndex, question, correctAnswer, allAnswers, addToF
         return newstr2;  
     }
 
-    const evaluateAnswer = (answer) => {
+    const evaluateAnswer = (answer, num) => {
+        eval(`setChecked${num}(true)`);
         if (answer !== correctAnswer) {
             setAnswerFeedback('Oooh, we\'re sorry! That\'s the wrong answer.');
-            setTimer(setTimeout(() => setAnswerFeedback(''), 3000))
+            setTimer(setTimeout(() => {
+                setNextQuestionBtnDisabled(false)
+                setAnswerFeedback('')
+                // formatIndex();
+                console.log('question 1', question)
+            }, 3000))
+            
             calculateScore(false)
         } else {
+            setNextQuestionBtnDisabled(false);
             setAnswerFeedback('That is correct! What, are you some kind of genius?');
-            setTimer(setTimeout(() => setAnswerFeedback(''), 3000))
+            setTimer(setTimeout(() => {
+                setNextQuestionBtnDisabled(false)
+                setAnswerFeedback('')
+                // formatIndex();
+                // clearCheckboxes();
+            }, 3000))
             calculateScore(true)
         }
+    }
+
+    const clearCheckboxes = () => {
+        setChecked1(false)
+        setChecked2(false)
+        setChecked3(false)
+        setChecked4(false)
     }
 
     const calculateScore = (answer) => {
@@ -85,34 +115,44 @@ const QuestionCard = ({currentIndex, question, correctAnswer, allAnswers, addToF
                 <div>
                     <input 
                     onChange={() => {
-                        evaluateAnswer(allAnswers[0])
+                        evaluateAnswer(allAnswers[0], 1)
                     }} 
-                    type="checkbox"/>
+                    type="checkbox"
+                    checked={checked1}
+                    />
                     <p>{cleanData(allAnswers[0])}</p>
                 </div>
                 <div>
                     <input 
-                    onChange={() => evaluateAnswer(allAnswers[1])} 
-                    type="checkbox"/>
+                    onChange={() => evaluateAnswer(allAnswers[1], 2)} 
+                    type="checkbox"
+                    checked={checked2}
+                    />
                     <p>{cleanData(allAnswers[1])}</p>
                 </div>
                 <div>
                     <input 
-                    onChange={() => evaluateAnswer(allAnswers[2])} 
-                    type="checkbox"/>
+                    onChange={() => evaluateAnswer(allAnswers[2], 3)} 
+                    type="checkbox"
+                    checked={checked3}
+                    />
                     <p>{cleanData(allAnswers[2])}</p>
                 </div>
                 <div>
                     <input 
-                    onChange={() => evaluateAnswer(allAnswers[3])} 
-                    type="checkbox"/>
+                    onChange={() => evaluateAnswer(allAnswers[3], 4)} 
+                    type="checkbox"
+                    checked={checked4}
+                    />
                     <p>{cleanData(allAnswers[3])}</p>
                 </div>
             </form>
             {!gameOver ?
             <Link to={`/question/${nextIndex}`}>
-                <button className="next-question-btn" onClick={() => {
-                checkResponse()
+                <button disabled={nextQuestionBtnDisabled}className="next-question-btn" onClick={() => {
+                // checkResponse()
+                // evaluateAnswer();
+                formatIndex();
                 }}
                 >Next Question</button>
             </Link> 
