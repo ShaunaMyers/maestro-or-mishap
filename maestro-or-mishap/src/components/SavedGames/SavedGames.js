bcb956bee72f93b87b78dc28ac3e568bbdcfddc2
Shauna Myers <shaunamyers26@gmail.com>
2021-08-09 13:16:37 +0100

    Add formatDate function

--- maestro-or-mishap/src/components/SavedGames/SavedGames.js
+++ maestro-or-mishap/src/components/SavedGames/SavedGames.js
@@ -1,17 +1,29 @@
 import { useState, useEffect } from "react";
 
-const SavedGames = ({questions, finalScore}) => {
+const SavedGames = ({finalScore}) => {
 
     const [savedGames, setSavedGames] = useState([]);
 
     useEffect(() => {
-        setSavedGames([...savedGames, {allQuestions: questions, finalScore, date: Date.now()}])
+        setSavedGames([...savedGames, {finalScore, date: Date.now()}])
     }, [])
+
+    const allSavedGames = savedGames.map(game => {
+        return (
+        <div>
+            <h4>Your game on {game.date}</h4>
+            <p>Score: {formatDate(game.formatDate)}</p>
+        </div>
+        )
+    })
+
+    const formatDate = (date) => {
+        return new Date(date).toLocaleString('en-US', { year: 'numeric', month: 'numeric', day: 'numeric' })
+    }
 
     return(
         <section>
-            <h4>Your Final Score</h4>
-            <p>Date: {calculateDate}</p>
+            {allSavedGames}
         </section>
     )
 }
