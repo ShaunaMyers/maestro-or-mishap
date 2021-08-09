3268f1dbfaf441aa2eb31dc5cc8b5dd6b6d6759e
Shauna Myers <shaunamyers26@gmail.com>
2021-08-09 22:58:34 +0100

    Remove handleDisplay question from Greeting

--- maestro-or-mishap/src/components/Greeting/Greeting.js
+++ maestro-or-mishap/src/components/Greeting/Greeting.js
@@ -1,12 +1,11 @@
 import { Link } from 'react-router-dom';
 import './Greeting.css';
 
-const Greeting = ({handleDisplayQuestion}) => {
+const Greeting = () => {
     return(
         <section className="greeting-container">
             <h2>Welcome!</h2>
             <p>Please try your hand at our music trivia game and see if you are a music maestro!</p>
-            {/* <button onClick={() => handleDisplayQuestion(0)}>Start Game!</button> */}
             <Link to='/question/0'><button>Start Game!</button></Link>
         </section>
     )
