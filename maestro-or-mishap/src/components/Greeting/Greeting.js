aed01306baa60a65d6f5eb4974fc41cb48123bfd
Shauna Myers <shaunamyers26@gmail.com>
2021-08-10 00:12:40 +0100

    Add button className

--- maestro-or-mishap/src/components/Greeting/Greeting.js
+++ maestro-or-mishap/src/components/Greeting/Greeting.js
@@ -6,7 +6,7 @@ const Greeting = () => {
         <section className="greeting-container">
             <h2>Welcome!</h2>
             <p>Please try your hand at our music trivia game and see if you are a music maestro!</p>
-            <Link to='/question/0'><button>Start Game!</button></Link>
+            <Link to='/question/0'><button className="start-game-btn">Start Game!</button></Link>
         </section>
     )
 }
