5072692ce31a848efd27339452b55495f7da2ea9
Shauna Myers <shaunamyers26@gmail.com>
2021-08-09 12:15:04 +0100

    Add finalScore to state w/ function to set it

--- maestro-or-mishap/src/components/App/App.js
+++ maestro-or-mishap/src/components/App/App.js
@@ -1,18 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { Route } from 'react-router-dom';
-import QuestionContainer from '../QuestionContainer/QuestionContainer'
-import './App.css';
 import { fetchQuestions } from '../../apiCalls';
+import './App.css';
+import QuestionContainer from '../QuestionContainer/QuestionContainer'
 import Greeting from '../Greeting/Greeting';
+import SavedGames from '../SavedGames/SavedGames';
 
 const App = () => {
   const [questions, setQuestions] = useState([]);
+  const [finalScore, setFinalScore] = useState(0);
 
   useEffect(() => {
     fetchQuestions() 
       .then(data => setQuestions(data.results))
       .catch(error => console.log(error))
   }, [])
+
+  const addToFinalScore = (score) => {
+    setFinalScore(score);
+  }
 
   return (
     <main className="App">
@@ -31,6 +37,11 @@ const App = () => {
         return(
           <QuestionContainer question={foundQuestion} currentIndex={index}/>
           )
+      }}/>
+      <Route exact path='/saved_games' render={() => {
+        return(
+          <SavedGames quesions={questions} finalScore={finalScore}/>
+        )
       }}/>
     </main>
   );
