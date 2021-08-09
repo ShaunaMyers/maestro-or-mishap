1c6672fb99443b74edee40be32408e009369cac3
Shauna Myers <shaunamyers26@gmail.com>
2021-08-09 10:28:58 +0100

    Add setTimer to state and setTimeOut func

--- maestro-or-mishap/src/components/QuestionCard/QuestionCard.js
+++ maestro-or-mishap/src/components/QuestionCard/QuestionCard.js
@@ -8,29 +8,47 @@ const QuestionCard = ({currentIndex, question, correctAnswer, allAnswers}) => {
     const [gameOver, setGameOver] = useState(false);
     const [nextIndex, setNextIndex] = useState(0);
     const [answerFeedback, setAnswerFeedback] = useState('');
+    const [timer, setTimer] = useState('');
+
+    const checkResponse = () => {
+        if (!answerFeedback) {
+            setAnswerFeedback('Please select an answer.');
+            setNextIndex(currentIndex);
+        } else {
+            setAnswerFeedback('');
+            formatIndex();
+        }
+    }
 
     const formatIndex = () => {
         if (currentIndex < 11) {
             const indexSummed = (currentIndex + 1);
-            setNextIndex(indexSummed)
+            setNextIndex(indexSummed);
         } else {
-            setNextIndex(11)
-            setGameOver(true)
+            setNextIndex(11);
+            setGameOver(true);
         }
     }
+
 
     const cleanData = (string) => {
         let re1 = /&quot;/gi;
         let re2 = /&#039;/gi;
-        let newstr1 = string.replace(re1, '"')
-        let newstr2 = newstr1.replace(re2,"'")
+        let newstr1 = string.replace(re1, '"');
+        let newstr2 = newstr1.replace(re2,"'");
         return newstr2;  
     }
 
     const evaluateAnswer = (answer) => {
-        answer !== correctAnswer ? setAnswerFeedback('Oooh, we\'re sorry! That\'s the wrong answer.') :
-        setAnswerFeedback('That is correct! What, are you some kind of genius?')
+        if (answer !== correctAnswer) {
+            setAnswerFeedback('Oooh, we\'re sorry! That\'s the wrong answer.');
+            setTimer(setTimeout(() => setAnswerFeedback(''), 3000))
+        } else {
+            setAnswerFeedback('That is correct! What, are you some kind of genius?');
+            setTimer(setTimeout(() => setAnswerFeedback(''), 3000))
+        }
     }
+
 
     return(
         <article className="question-card">
@@ -65,7 +83,8 @@ const QuestionCard = ({currentIndex, question, correctAnswer, allAnswers}) => {
             {!gameOver ?
             <Link to={`/question/${nextIndex}`}>
                 <button className="next-question-btn" onClick={() => {
-                formatIndex()
+                checkResponse()
+                // formatIndex()
                 }}
             >Next Question</button></Link> :
             <Link to={'/'}>
