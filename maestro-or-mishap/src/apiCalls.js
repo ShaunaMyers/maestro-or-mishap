d47ce3fee2ce53287fcd248233c3042bb8e28973
Shauna Myers <shaunamyers26@gmail.com>
2021-08-10 02:16:10 +0100

    Refactor console logged error

--- maestro-or-mishap/src/apiCalls.js
+++ maestro-or-mishap/src/apiCalls.js
@@ -2,7 +2,7 @@ export const fetchQuestions = () => {
     return fetch('https://opentdb.com/api.php?amount=12&category=12&difficulty=medium&type=multiple')
     .then(response => {
         if(!response.ok) {
-            throw Error('Error fetching reservations')
+            throw Error('Error fetching questions')
         }
         return response.json()
     })
